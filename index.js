d9b294e9a709649179a8e57847a7c9bc1d0adc3f
Felipe Martins <felipefmartins.contato@gmail.com>
2022-01-15 19:18:58 -0300

    Adicionando a funcao soma lista ao script js

--- index.js
+++ index.js
@@ -8,3 +8,9 @@ console.log(soma(n1, n2));
 const sub = (a, b) => a - b;
 
 console.log(sub(n2, n1));
+
+const somaLsita = (lista) => lista.reduce(soma);
+
+let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+console.log(somaLsita(soma));
